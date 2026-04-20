9acee1bdaa2676dcbfd581fa99cb0dfd740920d7
Afshan-Tayyab <tayyaba044705@gmail.com>
2026-04-20 16:29:40 +0530

    fix model

--- doctors/models.py
+++ doctors/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 class Doctor(models.Model):
     name = models.CharField(max_length=200)
-    specialty = models.CharField(max_length=200)  # ← Changed from specialization
+    specialty = models.CharField(max_length=200)  
     phone = models.CharField(max_length=15)
     email = models.EmailField()
-    availability = models.CharField(max_length=200, blank=True, null=True)  # ← Added
+    availability = models.CharField(max_length=200, blank=True, null=True)  
 
     def __str__(self):
         return self.name
